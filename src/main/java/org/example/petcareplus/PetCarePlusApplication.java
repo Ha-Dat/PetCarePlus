cd12779f4f96b63b69ff6a2a83d526cf895a80c2
Ha-Dat <hmd240203@gmail.com>
2025-06-11 18:51:13 +0700

    Initial commit

---
+++ src/main/java/org/example/petcareplus/PetCarePlusApplication.java
@@ -0,0 +1,13 @@
+package org.example.petcareplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCarePlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCarePlusApplication.class, args);
+    }
+
+}
