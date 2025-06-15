83e06cdc4d697b5c16985ebabcc135807a0b91a0
PheoGit <169252433+PheoGit@users.noreply.github.com>
2025-06-15 11:40:18 +0700

    Accept Pet Hotel Booking

--- src/main/java/org/example/petcareplus/controller/HotelBookingController.java
+++ src/main/java/org/example/petcareplus/controller/HotelBookingController.java
@@ -5,10 +5,10 @@ import org.example.petcareplus.repository.HotelBookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -25,5 +25,22 @@ public class HotelBookingController {
         List<HotelBooking> hotelBookings = hotelBookingRepository.findAll();
         model.addAttribute("hotelBookings", hotelBookings);
         return "groomer_hotel_page";
+    }
+
+    @PostMapping("/approve-booking/{id}")
+    @ResponseBody
+    public String approveBooking(@PathVariable("id") Integer id) {
+        Optional<HotelBooking> bookingOpt = hotelBookingRepository.findById(id);
+        if (bookingOpt.isPresent()) {
+            HotelBooking booking = bookingOpt.get();
+            if ("pending".equalsIgnoreCase(booking.getStatus())) {
+                booking.setStatus("accepted");
+                hotelBookingRepository.save(booking);
+                return "lịch đặt đã được duyệt";
+            } else {
+                return "lịch đặt đã được duyệt rồi";
+            }
+        }
+        return "Not found";
     }
 }
