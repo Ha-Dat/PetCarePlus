055d65ff560c024d23c33bf17262a68e89bc3980
Ha-Dat <hmd240203@gmail.com>
2025-06-17 00:15:32 +0700

    Add Approve Spa Booking

--- src/main/java/org/example/petcareplus/controller/SpaBookingController.java
+++ src/main/java/org/example/petcareplus/controller/SpaBookingController.java
@@ -6,10 +6,10 @@ import org.example.petcareplus.service.SpaBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/list-spa-booking")
@@ -24,12 +24,45 @@ public class SpaBookingController {
         this.spaBookingService = spaBookingService;
     }
 
-    @GetMapping("")
+    @GetMapping()
     public String getAllSpaBookings(Model model) {
         List<SpaBooking> spaBookings= spaBookingService.findAll();
         model.addAttribute("spaBookings", spaBookings);
         return "list-spa-booking";
     }
 
+    @PostMapping("/approve-spa/{id}")
+    @ResponseBody
+    public String approveSpaBooking(@PathVariable("id") Integer id){
+        Optional<SpaBooking> booking = spaBookingRepository.findById(id);
+        if (booking.isPresent()) {
+            SpaBooking spaBooking = booking.get();
+            if ("Chờ xác nhận".equalsIgnoreCase(spaBooking.getStatus())){
+                spaBooking.setStatus("Đã đặt");
+                spaBookingRepository.save(spaBooking);
+                return "Duyệt lịch thành công";
+            }else {
+                return "Lịch đặt đã được duyệt";
+            }
+        }
+        return "Không tìm thấy lịch";
+    }
+
+    @PostMapping("/reject-spa/{id}")
+    @ResponseBody
+    public String rejectSpaBooking(@PathVariable("id") Integer id){
+        Optional<SpaBooking> booking = spaBookingRepository.findById(id);
+        if (booking.isPresent()) {
+            SpaBooking spaBooking = booking.get();
+            if ("Chờ xác nhận".equalsIgnoreCase(spaBooking.getStatus())){
+                spaBooking.setStatus("Đã từ chối");
+                spaBookingRepository.save(spaBooking);
+                return "Từ chối lịch thành công";
+            }else {
+                return "Lịch đặt đã từ chối";
+            }
+        }
+        return "Không tìm thấy lịch";
+    }
 
 }
