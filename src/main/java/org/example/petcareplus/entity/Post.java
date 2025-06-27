7be4e0f8de4d8b05cdf858eec425ff6416e9aa80
PheoGit <169252433+PheoGit@users.noreply.github.com>
2025-06-27 22:00:24 +0700

    View List Post

--- src/main/java/org/example/petcareplus/entity/Post.java
+++ src/main/java/org/example/petcareplus/entity/Post.java
@@ -40,4 +40,76 @@ public class Post {
     private List<CommentPost> comments;
 
     //method thêm trong trường hợp lombok không hoạt động
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public Boolean getChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(Boolean checked) {
+        isChecked = checked;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<CommentPost> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentPost> comments) {
+        this.comments = comments;
+    }
 }
