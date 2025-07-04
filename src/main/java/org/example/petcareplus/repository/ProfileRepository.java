b13c58b8a01785cd8caa013a2b7292c826c6ff90
DungPham-git <anhdung6113@gmail.com>
2025-07-04 15:15:28 +0700

    Update ProfileRepository.java

--- src/main/java/org/example/petcareplus/repository/ProfileRepository.java
+++ src/main/java/org/example/petcareplus/repository/ProfileRepository.java
@@ -4,9 +4,9 @@ import org.example.petcareplus.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 import java.util.Optional;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
     Optional<Profile> findByAccount_AccountId(Integer accountId);
 
