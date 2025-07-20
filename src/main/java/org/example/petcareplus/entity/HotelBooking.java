99a2966a399ca4687a468fbf5fe984df795a8840
Nguyen Trung Duc <nguyentrungduc102003@gmail.com>
2025-07-20 20:40:34 +0700

    Update css for Hotel booking web

--- src/main/java/org/example/petcareplus/entity/HotelBooking.java
+++ src/main/java/org/example/petcareplus/entity/HotelBooking.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "HotelBookings")
+@Table(name = "hotel_bookings")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
