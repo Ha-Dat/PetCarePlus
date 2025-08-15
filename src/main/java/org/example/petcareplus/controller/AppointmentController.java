b5f6589b9e2fc4a84a3c94b9d51f2b4dc34951a1
Nguyen Trung Duc <nguyentrungduc102003@gmail.com>
2025-08-15 15:55:01 +0700

    Update dropdown for appointment.html

--- src/main/java/org/example/petcareplus/controller/AppointmentController.java
+++ src/main/java/org/example/petcareplus/controller/AppointmentController.java
@@ -29,6 +29,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Controller
+
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
