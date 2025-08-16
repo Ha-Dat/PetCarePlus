1cf80244d9ca08ab616bc08ba0ff708e9b77a57c
PheoGit <169252433+PheoGit@users.noreply.github.com>
2025-08-17 02:31:20 +0700

    update my-service-schedule.html

--- src/main/java/org/example/petcareplus/controller/ManagerController.java
+++ src/main/java/org/example/petcareplus/controller/ManagerController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-@RequestMapping("/manager")
+
 @Controller
 @RequestMapping("/manager")
 public class ManagerController {
